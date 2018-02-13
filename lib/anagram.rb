4a7b1c6069ac7b3fbb69a068c094c89f203e9d5e
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-13 19:03:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
   end
   
   def match(word_array)
-    word_array.collect do |word|
+    word_array.select do |word|
       word.split("").sort == @word.split("").sort
     end
   end
